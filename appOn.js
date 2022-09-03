1d0d3c6f6686f54ce43b9386a56744a84ab3bcf8
KaustubhBadvegit config --global user.name KaustubhBadvegit config --global user.name KaustubhBadvegit config --global user.name KaustubhBadve <kaustubhbadve7@gmail.com>
2022-09-03 10:23:52 +0530

    AAA

--- appOn.js
+++ appOn.js
@@ -13,15 +13,4 @@
 })();
 
 
-// export  const apptoggle =()=>{
-//     document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
-   
-//     let media = document.querySelectorAll("img, picture, video");
-//     media.forEach((mediaItem) => {
-//         mediaItem.style.filter = "invert(1) hue-rotate(180deg)"
-//     })
-// }
 
-// export  const backfround =()=>{
-   
-// }
